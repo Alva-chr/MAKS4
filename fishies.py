ae6542153f1c228355b196fd7d1783490d3a9353
Marcus Moritz <94021009+marcusmoritz@users.noreply.github.com>
2026-05-15 12:32:57 +0200

    Update fishies.py

--- fishies.py
+++ fishies.py
@@ -5,22 +5,37 @@ import random as r
 
 N = 500
 v0 = 0.03
-R = 0.1
+R=0.1
+n = 1
+eta = [0,0.05,0.1,pi/2,pi,2*pi]
+T = 10000
 
-
+# Initial Conditions
 x = np.random.rand(N)
 y = np.random.rand(N)
-
-angu = np.cos(np.linspace(0, 2*np.pi, N))
-angv = np.sin(np.linspace(0, 2*np.pi, N))
-
+angle = np.random.rand(N)*2*np.pi
+angle_temp = np.zeros_like(angle)
 
 
-plt.quiver(x,y,angu,angv)
-plt.xlim(0,1)
-plt.ylim(0,1)
+
+#Update the angle
+for step in range(0,T):
+    for i in range(N):
+        dx = x-x[i]
+        dy = y-y[i]
+        np.where((dx<0.5),dx+0.5,0)
+
+    
+
+    angle = (np.arctan2(sumx, sumy) + eta[0] * (np.random.random(N) - 0.5) * 2) % (2 * np.pi)
+
+    
+    x = (x+np.cos(angle))%1
+    y = (y+np.sin(angle))%1
 
 
-plt.gca().set_aspect('equal')
+    print(x[0])
+    print(y[0])
 
-plt.show()
+
+
