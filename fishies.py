77ba7d8fffe16a053fe650634a4a6cb080b39bae
alvaDenBasta <alvachristensson03@gmail.com>
2026-05-15 14:42:46 +0200

    added plotting for order parameter

--- fishies.py
+++ fishies.py
@@ -11,49 +11,65 @@ n = 1
 eta = [0,0.05,0.1,pi/2,pi,2*pi]
 T = 1000
 
-# Initial Conditions
-x = np.random.rand(N)
-y = np.random.rand(N)
-angle = np.random.rand(N)*2*np.pi
-angle_temp = np.zeros_like(angle)
 
-plt.ion()
-fig, ax = plt.subplots(figsize=(6,6))
-q = ax.quiver(x,y,np.cos(angle),np.sin(angle))
 
-#Update the angle
-for step in range(0,T):
-    sumx = np.zeros_like(x)
-    sumy = np.zeros_like(y)
-    for i in range(N):
-        dx = x-x[i]
-        dy = y-y[i]
-        condlistX = [dx<-0.5, dx>=0.5]
-        choicelistX = [dx+1, dx-1]
 
-        dx = np.select(condlistX,choicelistX,default=dx)
-        condlistY = [dy<-0.5, dy>=0.5]
-        choicelistY = [dy+1, dy-1]
 
-        dy = np.select(condlistY,choicelistY,default=dy)
+t_plot = np.arange(0,T)
 
-        dist_squared = dx*dx+dy*dy
 
-        mask = (dist_squared<R*R)
 
-        sumx[i] = np.sum(np.cos(angle[mask]))
-        sumy[i] = np.sum(np.sin(angle[mask]))
 
-    
+for e in eta:
+    theta_list = []
 
-    angle = (np.arctan2(sumy, sumx) + eta[0] * (np.random.random(N) - 0.5) * 2) % (2 * np.pi)
+    # Initial Conditions
+    x = np.random.rand(N)
+    y = np.random.rand(N)
+    angle = np.random.rand(N)*2*np.pi
+    angle_temp = np.zeros_like(angle)
+    theta_list.append(abs(np.sum(np.cos(angle)+np.sin(angle))/N))
+    # plt.ion()
+    # fig, ax = plt.subplots(figsize=(6,6))
+    # q = ax.quiver(x,y,np.cos(angle),np.sin(angle))
 
-    
-    x = (x+v0*np.cos(angle))%1
-    y = (y+v0*np.sin(angle))%1
+    for step in range(0,T):
+        sumx = np.zeros_like(x)
+        sumy = np.zeros_like(y)
+        for i in range(N):
+            dx = x-x[i]
+            dy = y-y[i]
+            condlistX = [dx<-0.5, dx>=0.5]
+            choicelistX = [dx+1, dx-1]
 
-    q.remove()
+            dx = np.select(condlistX,choicelistX,default=dx)
+            condlistY = [dy<-0.5, dy>=0.5]
+            choicelistY = [dy+1, dy-1]
 
-    q = ax.quiver(x, y, np.cos(angle), np.sin(angle))
+            dy = np.select(condlistY,choicelistY,default=dy)
 
-    plt.pause(0.001)
+            dist_squared = dx*dx+dy*dy
+
+            mask = (dist_squared<R*R)
+
+            sumx[i] = np.sum(np.cos(angle[mask]))
+            sumy[i] = np.sum(np.sin(angle[mask]))
+
+        angle = (np.arctan2(sumy, sumx) + e * (np.random.random(N) - 0.5) * 2) % (2 * np.pi)
+        
+        x = (x+v0*np.cos(angle))%1
+        y = (y+v0*np.sin(angle))%1
+
+        #q.remove()
+
+        #q = ax.quiver(x, y, np.cos(angle), np.sin(angle))
+
+        #plt.pause(0.001)
+
+        #plt.plot(theta_list, t_plot, label=str(eta))
+
+plt.xlabel("Time")
+plt.ylabel("$\Phi(t)$")
+plt.title("Order parameter for different values of $\eta$ over time")
+plt.legend()
+plt.show()
